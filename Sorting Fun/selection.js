2e103b4ed31853a86a05ea540a4821973383ebf3
krjayesh <krjayesh2004@gmail.com>
2025-06-06 15:29:27 +0530

    selection.js

---
+++ Sorting Fun/selection.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+
+    return arr;
+}
+
+const array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Sorted array: " + selectionSort(array));
