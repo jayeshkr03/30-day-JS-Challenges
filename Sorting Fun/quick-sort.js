fc2a0624f102cc0a22cecbd6c3ae673ec8865a1e
krjayesh <krjayesh2004@gmail.com>
2025-06-06 15:36:32 +0530

    sorted array using the Quick Sort algorithm.  * @param {number[]} arr - The array to be sorted.  * @returns {number[]} - The sorted array.  */

---
+++ Sorting Fun/quick-sort.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Sorted array: " + quickSort(array));
+
