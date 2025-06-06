d15bc6959711aba391c5a5114fb9ca151c10d842
krjayesh <krjayesh2004@gmail.com>
2025-06-06 18:23:23 +0530

    updated first-and-last-occurance.js

--- Search Party/first-and-last-occurance.js
+++ Search Party/first-and-last-occurance.js
@@ -1,34 +1,21 @@
+// Function to find the first and last index of a target in a sorted array
 function findFirstAndLastOccurrence(arr, target) {
     let first = -1;
     let last = -1;
 
     // Find first occurrence
-    let left = 0;
-    let right = arr.length - 1;
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (arr[mid] === target) {
-            first = mid;
-            right = mid - 1;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            first = i;
+            break; // Stop at the first match
         }
     }
 
     // Find last occurrence
-    left = 0;
-    right = arr.length - 1;
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (arr[mid] === target) {
-            last = mid;
-            left = mid + 1; 
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === target) {
+            last = i;
+            break; // Stop at the last match
         }
     }
 
@@ -38,11 +25,11 @@ function findFirstAndLastOccurrence(arr, target) {
 const sortedArray = [1, 2, 2, 3, 4, 4, 5, 6];
 const target = 4;
 const result = findFirstAndLastOccurrence(sortedArray, target);
+
 if (result.first !== -1 && result.last !== -1) {
     console.log(`First occurrence of ${target} is at index: ${result.first}`);
     console.log(`Last occurrence of ${target} is at index: ${result.last}`);
-}
-else {
+} else {
     console.log(`Element ${target} not found in the array.`);
 }
 
