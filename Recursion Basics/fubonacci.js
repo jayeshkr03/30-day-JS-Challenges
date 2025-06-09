2facd15234b833d215b20761607cbcfd9593de98
krjayesh <krjayesh2004@gmail.com>
2025-06-09 16:55:31 +0530

    fubonacci.js

--- Recursion Basics/fubonacci.js
+++ Recursion Basics/fubonacci.js
@@ -1,5 +1,10 @@
 // Fibonacci using Recursion
 
-function fiboRecursion {
-    
+function fibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+console.log(fibonacci(10))
