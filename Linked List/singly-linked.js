ad7a4e71b2557dd33b0db62b31d8ded31b2b8949
krjayesh03 <205539817+jayeshkr03@users.noreply.github.com>
2025-06-12 18:32:52 +0530

    single-linked

---
+++ Linked List/singly-linked.js
@@ -0,0 +1,32 @@
+// singly linked list
+
+class Node{
+  constructor(val){
+    this.val = val
+    this.next = null
+  }
+
+}
+
+class SinglyLinkedList{
+    constructor(){
+      this.head = null
+      this.tail = null
+      this.length = 0
+    }
+  
+  push(val){
+   let newNode = new Node(val)
+   
+   if(!this.head){
+       this.head = newNode
+       this.tail = this.head
+   } else {
+      this.tail.next = newNode
+      this.tail = newNode 
+    }
+    this.length++
+     this
+  }
+}
+
