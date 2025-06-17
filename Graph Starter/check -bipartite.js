296e0f5f26a5b6553c8fc1de88c118d9b1dc17c3
krjayesh03 <205539817+jayeshkr03@users.noreply.github.com>
2025-06-17 16:10:50 +0530

    simplify bipartite check implementation using array for color tracking

--- Graph Starter/check -bipartite.js
+++ Graph Starter/check -bipartite.js
@@ -1,37 +1,35 @@
 // Check bipartite
 
 function isBipartite(graph) {
-    const color = {};
+    const color = Array(graph.length).fill(null);
+    
+    function bfs(start) {
+        const queue = [start];
+        color[start] = 0;
 
-    for (let node in graph) {
-        if (!(node in color)) {
-            const queue = [node];
-            color[node] = 0;
-
-            while (queue.length > 0) {
-                const current = queue.shift();
-
-                for (let neighbor of graph[current]) {
-                    if (!(neighbor in color)) {
-                        color[neighbor] = 1 - color[current]; 
-                        queue.push(neighbor);
-                    } else if (color[neighbor] === color[current]) {
-                        return false; 
-                    }
+        while (queue.length > 0) {
+            const node = queue.shift();
+            for (const neighbor of graph[node]) {
+                if (color[neighbor] === null) {
+                    color[neighbor] = 1 - color[node];
+                    queue.push(neighbor);
+                } else if (color[neighbor] === color[node]) {
+                    return false;
                 }
             }
+        }
+
+        return true;
+    }
+
+    for (let i = 0; i < graph.length; i++) {
+        if (color[i] === null) {
+            if (!bfs(i)) return false;
         }
     }
 
     return true;
 }
 
-
-const graph1 = {
-    A: ['B', 'D'],
-    B: ['A', 'C'],
-    C: ['B', 'D'],
-    D: ['A', 'C']
-};
-
-console.log(isBipartite(graph1)); 
+console.log(isBipartite([[1,3],[0,2],[1,3],[0,2]])); 
+console.log(isBipartite([[1,2,3],[0,2],[0,1,3],[0,2]])); 
