4e39fd5f65d01eadb4c8c53e7ee7c69abfc4f113
krjayesh03 <205539817+jayeshkr03@users.noreply.github.com>
2025-06-18 11:57:37 +0530

    Fibonacci and Climb Stairs algorithms using dynamic programming

---
+++ Dyanamic Programming 1/climb-stairs.js
@@ -0,0 +1,15 @@
+// climb stairs using dynamic programming
+
+function climbStairs(n) {
+  if (n <= 2) return n;
+
+  let arr = [0, 1, 2];
+
+  for (let i = 3; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  return arr[n];
+}
+
+console.log(climbStairs(5));
--- Dyanamic Programming 1/fibonacci.js
+++ Dyanamic Programming 1/fibonacci.js
@@ -1,2 +1,16 @@
 // fibonacci using dynamic programming
 
+function fibonacci(n) {
+  if (n <= 1) return n;
+
+  let arr = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  return arr[n];
+}
+
+console.log(fibonacci(15)); 
+\console.log(fibonacci(17));  
