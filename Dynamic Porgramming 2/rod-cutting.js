1922545d4a57c23d4c0ae7c3d9949d1c15be1128
krjayesh03 <205539817+jayeshkr03@users.noreply.github.com>
2025-06-19 19:09:20 +0530

    rod cutting

--- Dynamic Porgramming 2/rod-cutting.js
+++ Dynamic Porgramming 2/rod-cutting.js
@@ -1,15 +1,16 @@
 // Rod Cutting Problem
 
-function rodCutting(prices, n) {
-  let arr = new Array(n + 1).fill(0);
+function cutRod(prices, length) {
+  let box = new Array(length + 1).fill(0);
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= length; i++) {
     for (let j = 0; j < i; j++) {
-      arr[i] = Math.max(arr[i], prices[j] + arr[i - j - 1]);
+      box[i] = Math.max(box[i], prices[j] + box[i - j - 1]);
     }
   }
 
-  return arr[n];
+  return box[length];
 }
 
-console.log(rodCutting([1, 5, 8, 9, 10], 4)); 
+console.log(cutRod([1, 5, 8, 9, 10], 4));
+console.log(cutRod([2, 5, 7, 8], 4));
